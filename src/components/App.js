9829ead8df9ab53689637356fe6b35809d81a43a
Ismael Boro <ismaelboro@Ismaels-MBP.home>
2020-10-10 11:34:40 -0400

    tic tac toe game completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,41 @@
 import React, {Component} from 'react';
-
+import Header from '../components/Header.js';
+import Board from '../components/Board.js';
+import img0 from '../images/0.png';
+import img1 from '../images/1.png';
+import Cell from './Cell.js';
+import Info from './Info.js';
 
 class Game extends Component {
 
+
+  // if( this.state.gamestae)
+  state={
+    visible:false, buttonStart:false
+};
+
   render(){
-    return ();
+   var enterGame =<button className='btn btn-dark niceFont buttonCentered' style={{display : "block" }} onClick={()=>{this.setState({visible:true}); }}>Start</button>
+   var startGame= <button onClick={()=>{this.setState({buttonStart:true}); }} className='btn btn-dark niceFont buttonCentered'>Start Game</button>;
+   var playerNames=<div className="makeFlex"><img src={img0} alt="missed" className="icon left" style={{display : "block" }}></img>ok<img src={img1} alt="missed" className='icon right' style={{display : "block" }}></img>cool</div>;
+   var resetAndNew =<div><button className="btn btn-dark niceFont left">Restart </button> <button  className="btn btn-dark niceFont right" onClick={refreshPage}>New Game </button>   </div>
+ //function for new game 
+   function refreshPage() {
+    window.location.reload(false);
+    
+  }   
+    return (
+  <div className='container'>
+   {!this.state.visible && !this.state.buttonStart ? <><Header/>  {enterGame}</>: null }
+   {this.state.visible && !this.state.buttonStart ?<><Header/><Info/> <>{startGame}</></>: null }
+   {this.state.buttonStart ?<><Header/> <>{playerNames}</><Board/> {resetAndNew} </>: null }
+ 
+    
+    {/* the game started ? <Board/> : null */}
+    {/* <Cell/> */}
+    {/* <Info/> */}
+  </div>
+);
   }
 }
 
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,88 @@
 import React, {Component}  from 'react';
-
+import Cell from '../components/Cell';
+  
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cells: Array(9).fill(null),
+       xNextPlayer: true,
 
+    };
+}
+
+ handleClick(i) {
+    const cells = this.state.cells.slice();
+    if (calculateWinner(cells) || cells[i]) {
+      return;
+    }
+    cells[i] = this.state.xNextPlayer ? 'X' : 'O';
+    this.setState({
+      cells: cells,
+      xNextPlayer: !this.state.xNextPlayer,
+    });
+  }
+
+  renderCell(i) {
+    return (
+       <Cell
+          value={this.state.cells[i]} 
+          onClick={() => this.handleClick(i)}
+       />
+
+    );
+  }
   render(){
-      return();
+      const winner = calculateWinner(this.state.cells);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xNextPlayer ? 'X' : 'O');
+    }
+      return(
+          <div className='Board middle ' >
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderCell(0)}
+          {this.renderCell(1)}
+          {this.renderCell(2)}
+        </div>
+        <div className="board-row">
+          {this.renderCell(3)}
+          {this.renderCell(4)}
+          {this.renderCell(5)}
+        </div>
+        <div className="board-row">
+          {this.renderCell(6)}
+          {this.renderCell(7)}
+          {this.renderCell(8)}
+        </div>
+      </div>
+    );
   }
 }
 
 export default Board;
+// import Board from '../components/Board.js';
+// import img0 from '../images/0.png';
+function calculateWinner(cells) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+      return cells[a];
+    }
+  }
+  return null;
+}
